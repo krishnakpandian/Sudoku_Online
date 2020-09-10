3a0d18210262516dce2a58c3740d8b066abc943b
kpandian <kpandian@ucsc.edu>
2020-09-09 19:25:22 -0700

    Firebase stuff

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,60 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flask_socketio import SocketIO, send
+from firebase_admin import credentials, firestore, initialize_app
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return {"post": "Hello World!"}
+cred = credentials.Certificate('serviceKey.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+todo_ref = db.collection('messages')
+
+@app.route('/add', methods=['POST'])
+def create():
+    """
+        create() : Add document to Firestore collection with request body.
+        Ensure you pass a custom ID as part of json body in post request,
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:
+        messages = request.json['messages']
+        todo_ref.document().set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/list', methods=['GET'])
+def read():
+    try:
+        # Check if ID was passed to URL query
+        todo_id = request.args.get('id')
+        if todo_id:
+            todo = todo_ref.document(todo_id).get()
+            return jsonify(todo.to_dict()), 200
+        else:
+            all_todos = [doc.to_dict() for doc in todo_ref.stream()]
+            return jsonify(all_todos), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/update', methods=['POST', 'PUT'])
+def update():
+    try:
+        id = request.json['id']
+        todo_ref.document(id).update(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+@app.route('/delete', methods=['GET', 'DELETE'])
+def delete():
+    try:
+        # Check for ID in URL query
+        todo_id = request.args.get('id')
+        todo_ref.document(todo_id).delete()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 socketIo = SocketIO(app, cors_allowed_origins="*")
